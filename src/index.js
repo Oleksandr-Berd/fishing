b133448620565e5f72b62bedcccb9bd22c0be7f3
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-02-26 16:15:32 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import temp1 from "./js/temp1";
 import temp2 from "./js/temp2";
+// import "./css/temp1.css";
+import "./sass/main.scss";
 
 console.log(temp1);
 console.log(temp2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,49 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
+  plugins: [new MiniCssExtractPlugin({ filename: "css/styles.css" })],
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    path: path.resolve(__dirname, "public"),
+    filename: "js/app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
   },
 };
