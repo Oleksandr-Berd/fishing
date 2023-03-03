4dda66b8ee29d372c62680684adf6390a845342e
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-03-03 13:05:06 +0200

    1

--- frontend/src/Components/FishingLocations/PreciseLocation/PreciseLocation.jsx
+++ frontend/src/Components/FishingLocations/PreciseLocation/PreciseLocation.jsx
@@ -7,18 +7,34 @@ import { getLocById } from "../../../Utilities/Regions/getRegions";
 
 export const PreciseLocation = () => {
   const { _id, path } = useParams();
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState("");
 
   useEffect(() => {
     getLocById(_id, path).then(setLocation);
   }, [_id, path]);
+
+  const {
+    title,
+    adress,
+    fishes,
+    description,
+    coordinates,
+    allowed_time,
+    fishing_conditions,
+    picture,
+  } = location;
+
   return (
     <div>
       <ButtonContainer>
         <BackButton />
         <HomeButton />
       </ButtonContainer>
-      <p>Precise location{location !== null && location.title}</p>
+      <h1>{location !== null && title}</h1>
+      <p>Adress: {adress}</p>
+      <p>{description}</p>
+      <p>Fishing conditions: {fishing_conditions}</p>
+      <p>Allowed time for fishing: {allowed_time}</p>
     </div>
   );
 };
