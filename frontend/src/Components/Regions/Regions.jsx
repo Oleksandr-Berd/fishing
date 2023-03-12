ff246e46f38675b204d61181d03ed64ea2cfac89
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-03-12 08:13:22 +0200

    1

--- frontend/src/Components/Regions/Regions.jsx
+++ frontend/src/Components/Regions/Regions.jsx
@@ -5,7 +5,10 @@ import { BaseUrlPicture } from "../../Utilities/Regions/URL";
 
 export const Regions = ({ regions, loading }) => {
   const location = useLocation();
-
+  const arrayRegions = regions.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  console.log(arrayRegions);
   return (
     <ul className={css.regionsList}>
       {loading && (
@@ -18,23 +21,25 @@ export const Regions = ({ regions, loading }) => {
           wrapperClass="dna-wrapper"
         />
       )}
-      {regions.map(({ _id, name, locPath, image }) => (
-        <div key={_id} className={css.container}>
-          <Link
-            key={_id}
-            to={`/region/${locPath}`}
-            state={{ from: location }}
-            className={css.link}
-          >
-            <img
-              src={`${BaseUrlPicture}/${image[0]}`}
-              alt=""
-              className={css.image}
-            />
-            <h1 className={css.regionsItem}> {name}</h1>
-          </Link>
-        </div>
-      ))}
+      {regions
+        .sort((a, b) => b.name - a.name)
+        .map(({ _id, name, locPath, image }) => (
+          <div key={_id} className={css.container}>
+            <Link
+              key={_id}
+              to={`/region/${locPath}`}
+              state={{ from: location }}
+              className={css.link}
+            >
+              <img
+                src={`${BaseUrlPicture}/${image[0]}`}
+                alt=""
+                className={css.image}
+              />
+              <h1 className={css.regionsItem}> {name}</h1>
+            </Link>
+          </div>
+        ))}
     </ul>
   );
 };
