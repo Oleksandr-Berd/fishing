3f9ee7ff01073bfa6dec13e530beabd5a71764ec
Oleksandr <alex.berd86@gmail.com>
2023-03-14 18:47:36 +0200

    Update Footer.jsx

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -8,14 +8,7 @@ export const Footer = () => {
     <footer className={css.footer}>
       <p style={{ marginBottom: "0" }}>© Copyright Fishing 2023 test</p>
       <address className={css.adress}>
-        <ul className={css.adressList}>
-          {/* <li>
-            <a href="mailto:alex.berd86@gmail.com">alex.berd86@gmail.com</a>
-          </li>
-          <li>
-            <a href="tel:+380673588786">+38 067 358 87 86</a>
-          </li> */}
-        </ul>
+        <ul className={css.adressList}></ul>
         <ul className={css.socList}>
           <li className={css.socItem}>
             <SocialIcon
