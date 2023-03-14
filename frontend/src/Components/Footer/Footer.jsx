e1704235c1e21451a0f6b45d08c5a8fcdb890ef9
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-03-14 17:51:27 +0200

    add soc networks in footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,3 +1,6 @@
+import { SocialIcon } from "react-social-icons";
+import React from "react";
+
 import css from "./Footer.module.css";
 
 export const Footer = () => {
@@ -11,6 +14,36 @@ export const Footer = () => {
           </li>
           <li>
             <a href="tel:+380673588786">+38 067 358 87 86</a>
+          </li>
+        </ul>
+        <ul className={css.socList}>
+          <li className={css.socItem}>
+            <SocialIcon
+              url="https://www.facebook.com/sasha.berdichevsky"
+              style={{ width: 36, height: 36 }}
+              fgColor="#fff"
+            />
+          </li>
+          <li className={css.socItem}>
+            <SocialIcon
+              url="https://www.linkedin.com/in/oleksandr-berdychevskyi-83336761/"
+              style={{ width: 36, height: 36 }}
+              fgColor="#fff"
+            />
+          </li>
+          <li className={css.socItem}>
+            <SocialIcon
+              url="https://github.com/Oleksandr-Berd"
+              style={{ width: 36, height: 36 }}
+              fgColor="#fff"
+            />
+          </li>
+          <li className={css.socItem}>
+            <SocialIcon
+              url="https://twitter.com/UaBerd"
+              style={{ width: 36, height: 36 }}
+              fgColor="#fff"
+            />
           </li>
         </ul>
       </address>
